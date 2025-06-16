3c592af5c498943dad2adab520e8741d57c2b2a3
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2025-06-16 22:11:21 +0700

    api : logout, create, update, delete data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,15 @@ class DatabaseSeeder extends Seeder
             'is_admin' => true,
         ]);
 
+        User::factory()->create([
+            'name' => 'Musthafa Luthfi',
+            'email' => 'musthafaluthfi@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'is_admin' => true,
+        ]);
+
         User::factory(100)->create();
         Category::factory(200)->create();
         Todo::factory(500)->create();
---
+++ tugas12.md
@@ -0,0 +1,23 @@
+Logout:
+
+![alt text](screenshot/tugas11/Logout.png)
+
+GetAllTodo :
+
+![alt text](screenshot/tugas11/GetAllTodo.png)
+
+TambahTodo :
+
+![alt text](<screenshot/tugas11/Tambah Todo.png>)
+
+UpdateTodo :
+
+![alt text](screenshot/tugas11/UpdateTodo.png)
+
+HapusTodo :
+
+![alt text](screenshot/tugas11/HapusTodo.png)
+
+SearchTodo :
+
+![alt text](screenshot/tugas11/SearchTodo.png)
